848a386ce60f1411e052c157487a2eb0b19e913a
Máximo Cuadros <mcuadros@gmail.com>
2015-06-21 20:54:27 +0200

    channels, funcs and var

--- reflection_test.go
+++ reflection_test.go
@@ -25,6 +25,24 @@ func (s *S) SetUpTest(c *C) {
 	}
 }
 
+func (s *S) BenchmarkInitVar(c *C) {
+	var value int
+	for i := 0; i < c.N; i++ {
+		value = 42
+	}
+
+	_ = value
+}
+
+func (s *S) BenchmarkInitVarReflect(c *C) {
+	var value reflect.Value
+	for i := 0; i < c.N; i++ {
+		value = reflect.ValueOf(42)
+	}
+
+	_ = value
+}
+
 func (s *S) BenchmarkMakeSliceAndFill(c *C) {
 	for i := 0; i < c.N; i++ {
 		value := make([]int, 0, 0)
@@ -36,7 +54,7 @@ func (s *S) BenchmarkMakeSliceAndFill(c *C) {
 
 func (s *S) BenchmarkMakeSliceAndFillReflect(c *C) {
 	for i := 0; i < c.N; i++ {
-		value := reflect.MakeSlice(reflect.SliceOf(reflect.ValueOf(42).Type()), 0, 0)
+		value := reflect.MakeSlice(reflect.SliceOf(reflect.TypeOf(42)), 0, 0)
 		for i := 0; i < 100; i++ {
 			value = reflect.Append(value, reflect.ValueOf(42))
 		}
@@ -54,7 +72,7 @@ func (s *S) BenchmarkMakeMapAndFill(c *C) {
 
 func (s *S) BenchmarkMakeMapAndFillReflect(c *C) {
 	for i := 0; i < c.N; i++ {
-		T := reflect.ValueOf(42).Type()
+		T := reflect.TypeOf(42)
 		value := reflect.MakeMap(reflect.MapOf(T, T))
 		for i := 0; i < 100; i++ {
 			value.SetMapIndex(
@@ -63,4 +81,45 @@ func (s *S) BenchmarkMakeMapAndFillReflect(c *C) {
 			)
 		}
 	}
+}
+
+func (s *S) BenchmarkCallFunc(c *C) {
+	for i := 0; i < c.N; i++ {
+		multiply(42, 42)
+	}
+}
+
+func (s *S) BenchmarkCallFuncReflect(c *C) {
+	for i := 0; i < c.N; i++ {
+		fn := reflect.ValueOf(multiply)
+		fn.Call([]reflect.Value{reflect.ValueOf(42), reflect.ValueOf(42)})
+	}
+}
+
+func (s *S) BenchmarkMakeChanAndPut(c *C) {
+	for i := 0; i < c.N; i++ {
+		ch := make(chan int)
+		go func() {
+			_ = <-ch
+		}()
+
+		ch <- 42
+		close(ch)
+	}
+}
+
+func (s *S) BenchmarkMakeChanAndPutReflect(c *C) {
+	for i := 0; i < c.N; i++ {
+		ch := reflect.MakeChan(reflect.TypeOf(make(chan int)), 0)
+		go func() {
+			ch.Recv()
+		}()
+
+		ch.Send(reflect.ValueOf(42))
+		ch.Close()
+	}
+}
+
+func multiply(a, b int) int {
+	return a * b
 }
